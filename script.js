3e2930cf6cabb5391e6bf560f2c4d78190d34692
Dezhang1999 <1162838174@qq.com>
2018-04-26 08:23:02 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+//alert("Welcome to the pandora box");
+//alert("This box will give you anything you want");
+//alert("That is created by Dequan");
+function giphyURLWithSearchTerm(searchTerm) {
+    
+    return "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC";
+    
+
+}
+
+function appendImageToBody(srcURL) {
+
+    $('body').append('<img src=' + srcURL + '>');
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm ) {
+
+    var t = giphyURLWithSearchTerm(searchTerm);
+
+    $.ajax({
+      url: t,
+      method: "GET",
+      success: function(response) {
+        var a = Math.floor((Math.random()*response.data.length)+0);
+        var b = response.data.length-Math.floor((Math.random() * 10) + 1);
+        var c = Math.floor((Math.random()*10)+1);
+        for(var n=a; n <=b; n=n+c){
+           var url = response.data[n].images.original.url;
+           appendImageToBody(url);}
+           console.log(a,b);
+      },
+    }); 
+}
+function imageclear(){
+    $("img").hide();
+}
+
+var count = 0;
+
+$("button").click(function(n){
+    imageclear();
+    var search = $("input").val();
+    giphyURLWithSearchTerm(search);
+    callGiphyAPIWithSearchTerm(search);
+    if(count ===1){
+       
+    }
+});
